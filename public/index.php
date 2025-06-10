e19119573a721026c44843f0f464a4909e68cc48
Tychyna Volodymyr <tychinavova@gmail.com>
2025-06-10 14:31:34 +0300

    Fix db config filename

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 // Подключаем конфиги
 require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../config/database.php';
 
 // Автозагрузка классов (пока простой вариант)
 spl_autoload_register(function ($class) {
